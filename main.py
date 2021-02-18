46ede16d4bd4a2d7a9a9aaa2dbda1693e9f4d530
Cedric Hopf <hopf.cedric@gmail.com>
2021-02-18 14:23:44 +0100

    Initialize status script

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import os, requests
+from config import config
+from github import github
+from drone import build
+
 def main():
-  print("Hello there!")
+  cfg = config.Config()
+  client = github.GitHubClient(cfg.api_token, cfg.endpoint, cfg.repository, cfg.commit)
+  drone = build.DroneBuild(cfg.drone_host, cfg.drone_protocol, cfg.drone_repository, cfg.drone_build)
+  body = {
+    "state": os.getenv("PLUGIN_STATE"),
+    "target_url": drone.generate_target_url(),
+    "description": os.getenv("PLUGIN_DESCRIPTION"),
+    "context": os.getenv("PLUGIN_CONTEXT")
+  }
+  client.create_status(body)
 
 main()
