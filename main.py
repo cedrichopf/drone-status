5e41e56f5fccb6e7cb3acff387afca6b30244a8b
Cedric Hopf <hopf.cedric@gmail.com>
2021-02-19 16:34:11 +0100

    Add try-except block

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import os, requests
+import sys, os
 from config import config
 from github import github
 from drone import build
 
 def main():
-  cfg = config.Config()
-  client = github.GitHubClient(cfg.api_token, cfg.endpoint, cfg.repository, cfg.commit)
-  drone = build.DroneBuild(cfg.drone_host, cfg.drone_protocol, cfg.drone_repository, cfg.drone_build)
-  body = {
-    "state": os.getenv("PLUGIN_STATE"),
-    "target_url": drone.generate_target_url(),
-    "description": os.getenv("PLUGIN_DESCRIPTION"),
-    "context": os.getenv("PLUGIN_CONTEXT")
-  }
-  client.create_status(body)
+  try:
+    cfg = config.Config()
+    client = github.GitHubClient(cfg.api_token, cfg.endpoint, cfg.repository, cfg.commit)
+    drone = build.DroneBuild(cfg.drone_host, cfg.drone_protocol, cfg.drone_repository, cfg.drone_build)
+    body = {
+      "state": os.getenv("PLUGIN_STATE"),
+      "target_url": drone.generate_target_url(),
+      "description": os.getenv("PLUGIN_DESCRIPTION"),
+      "context": os.getenv("PLUGIN_CONTEXT")
+    }
+    client.create_status(body)
+  except Exception as err:
+    sys.exit(err)
 
 main()
